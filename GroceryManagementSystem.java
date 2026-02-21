b1ba61ab1de001c1152ee8a6170d80b6f1f00095
Jonhatan123-hub <jonhatanmartinez244@gmail.com>
2026-02-20 22:01:47 -0600

    Implement printInventory method for item listing
    Adds a method to print the inventory of items, filtering out null names.

---
+++ GroceryManagementSystem.java
@@ -0,0 +1,22 @@
+/**
+     * Prints all items currently in the inventory.
+     * Only prints items whose name is not null.
+     *
+     * @param names  array of item names
+     * @param prices array of item prices
+     * @param stocks array of item stock quantities
+     */
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+
+        System.out.println("\n--- Inventory List ---");
+
+        for (int i = 0; i < names.length; i++) {
+
+            if (names[i] != null) {
+                System.out.println(
+                        "Item: " + names[i] +
+                                " | Price: $" + prices[i] +
+                                " | Stock: " + stocks[i]);
+            }
+        }
+    }
